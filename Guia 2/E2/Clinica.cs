b906c4f19fcf797369f769d3b24062e4a0edc8e9
AbigailVictoriaMedinaBruno <abigailmedinabruno@gmail.com>
2020-04-20 09:50:47 -0300

    Problemas Ejercicio 2

---
+++ Guia 2/E2/Clinica.cs
@@ -0,0 +1,12 @@
+namespace E2
+{
+    public class Clinica
+    {
+        List<Medico> medicos = new List<Medico>();
+        Medico gustavo = new Medico("Gustavo","Parelli","Pediatria",32);
+        
+        medicos.Add(gustavo);
+
+
+    }
+}
--- Guia 2/E2/Program.cs
+++ Guia 2/E2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace E2
 {
     class Program
