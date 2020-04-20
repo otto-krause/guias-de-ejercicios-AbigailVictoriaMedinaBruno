753a0c00b67dbe0b58374f82aeb312262348a570
AbigailVictoriaMedinaBruno <abigailmedinabruno@gmail.com>
2020-04-20 13:52:28 -0300

    2do error en el ejercicio 2

--- Guia 2/E2/Clinica.cs
+++ Guia 2/E2/Clinica.cs
@@ -2,10 +2,19 @@ namespace E2
 {
     public class Clinica
     {
-        List<Medico> medicos = new List<Medico>();
-        Medico gustavo = new Medico("Gustavo","Parelli","Pediatria",32);
+        List<Medico> medicos=new System.Collections.Generic.List<Medico>();
+        public Clinica(){
+            
+            Medico gustavo = new Medico("Gustavo","Parelli","Pediatria",32);
+            Medico patricia = new Medico("Patricia","White","Pediatria",100);
+            Medico luciano = new Medico("Luciano","Maddox","Obstetricia",45);
+            Medico federico = new Medico("Federico","Micelo","Odontologia",60);
+            medicos.Add(gustavo);
+            medicos.Add(patricia);
+            medicos.Add(luciano);
+            medicos.Add(federico);
+        }
         
-        medicos.Add(gustavo);
 
 
     }
