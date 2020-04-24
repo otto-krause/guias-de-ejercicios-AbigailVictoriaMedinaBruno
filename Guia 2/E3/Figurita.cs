a1ed8c17958a10f62624e20c5cb8bf4da2ecd449
AbigailVictoriaMedinaBruno <abigailmedinabruno@gmail.com>
2020-04-24 18:13:12 -0300

    Ejercicio 3 hecho

---
+++ Guia 2/E3/Figurita.cs
@@ -0,0 +1,17 @@
+
+namespace E3
+{
+    public class Figurita
+    {
+        public string nombre,posicion,pais;
+        public int numeroDeFigurita=0;
+        public Figurita(string nombre,string posicion,string pais,int numeroDeFigurita){
+            this.nombre=nombre;
+            this.posicion=posicion;
+            this.pais=pais;
+            this.numeroDeFigurita=numeroDeFigurita;
+        }
+        //dou
+
+    }
+}
