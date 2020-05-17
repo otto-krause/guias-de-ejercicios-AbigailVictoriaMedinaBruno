de037390f9557119d038fadc04307477b5822292
luciacenturion <centurionluciayael@gmail.com>
2020-05-16 22:39:17 -0300

    Ejercicio 4 Hecho

--- Guia 3/E4/Program.cs
+++ Guia 3/E4/Program.cs
@@ -4,9 +4,53 @@ namespace E4
 {
     class Program
     {
+        static void elTragalibros(Tragalibros persona){
+            int opcion=0;
+            string titulo;
+            string autor;
+            do{
+                Console.WriteLine("\n¿Qué desea hacer?"+
+                "\n(1)Leer un libro"+
+                "\n(2)Calcular CI"+
+                "\nIngrese cualquier otra tecla para salir.");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch(opcion){
+                    case 1:
+                        Console.WriteLine("Ingrese el titulo del libro:");
+                        titulo = Console.ReadLine();
+                        Console.WriteLine("Ingrese el autor del libro:");
+                        autor = Console.ReadLine();
+                        persona.leer(new Libro(titulo,autor));
+                        break;
+                    case 2:
+                        Console.WriteLine("Su CI es: "+persona.calcularCI());
+                        break;
+                    default: opcion=0;
+                        break;
+                }
+            }while(opcion != 0);    
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Cualquier ejemplo utilizado tal vez no tiene ninguna relación con la realidad
+            Tragalibros Gabriel = new Tragalibros();
+            Tragalibros Fabricio = new Tragalibros();
+            int opcion=0;
+            do{    
+                Console.WriteLine("\n¿A quién atendemos?"+
+                "\n(1)Gabriel."+
+                "\n(2)Fabricio."+
+                "\nIngrese cualquier otra tecla para salir.");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch(opcion){
+                    case 1:elTragalibros(Gabriel);
+                        break;
+                    case 2:elTragalibros(Fabricio);
+                        break;
+                    default: opcion=0;
+                        break;
+                }
+            }while(opcion!=0);
         }
     }
 }
---
+++ Guia 3/E4/Tragalibros.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+namespace E4
+{
+    public class Tragalibros
+    {
+        List<Libro> listaDeLibrosLeidos = new List<Libro>();
+        public Tragalibros(){
+        }
+        public void leer(Libro libro){
+            int contador = 0;
+            foreach(Libro libroAux in listaDeLibrosLeidos){
+                if(libro.Titulo == libroAux.Titulo && libro.Autor == libroAux.Autor)
+                    contador++;
+            }
+            if(contador == 0)
+                listaDeLibrosLeidos.Add(libro);
+        }
+        public int calcularCI(){
+            int CI=listaDeLibrosLeidos.Count;
+            return 5*CI;
+        }
+    }
+}
