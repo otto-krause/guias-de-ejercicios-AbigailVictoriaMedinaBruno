97b5d7763fe84ff84a878ee8b7ceaec934cca3f0
AbigailVictoriaMedinaBruno <abigailmedinabruno@gmail.com>
2020-07-10 22:01:34 -0300

    Ejercicio 5 hecho

--- Guia 5/E5/Program.cs
+++ Guia 5/E5/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace E5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> conocimientos1 = new List<string>{"agua","bol","depilación","cartillo","expiración","celeste","falso","verdadero"};
+            //List<string> conocimientos2 = new List<string>{"agua","fornite","pepe","celeste","amistad","azul","celular","amor"};
+            //List<string> conocimientos3 = new List<string>{"respirar","comer","cerrar","tinte","morado","nueve","ocho","liso"};
+
+            Persona pablo = new Persona(conocimientos1);
+
+            Console.WriteLine("\nLos Ultimos 5 conocimientos adquiridos:");
+            pablo.ultimos5Conocimientos().ForEach(i => Console.WriteLine(i));
+
+            Console.WriteLine("\nLos primeros 4 conocimientos en orden alfabetico:");
+            pablo.primeros4Conocimientos().ForEach(j => Console.WriteLine(j));
+
+            Console.WriteLine("\n"+ pablo.cuantosTienenCion() +" palabras contienen 'ción'.");
+            
+
+
         }
     }
 }
