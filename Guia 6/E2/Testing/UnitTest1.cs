c900da3360a03f733626124eee280b14ade877b4
AbigailVictoriaMedinaBruno <abigailmedinabruno@gmail.com>
2020-08-14 13:37:49 -0300

    Ejercicio 2 hecho

--- Guia 6/E2/Testing/UnitTest1.cs
+++ Guia 6/E2/Testing/UnitTest1.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
-
+using Ejercicio;
+using System.Collections.Generic;
+using System.Linq;
 namespace Testing
 {
     public class Tests
     {
+        Mago HarryPostre;
+        Carpe varitaCarpe;
+        Receta receta1; 
+        //Receta receta2;
         [SetUp]
         public void Setup()
         {
+            receta1 = new Receta(3,new List<int>{400,400,400});
+            varitaCarpe = new Carpe(10);
+            HarryPostre = new Mago(varitaCarpe,new List<Receta>{receta1,receta1});
         }
 
         [Test]
-        public void Test1()
+        public void TestSaberSiTodasLasRecetasSonDeGordoDeHarryPostre()
         {
-            Assert.Pass();
+            Assert.AreEqual(false,HarryPostre.noSeCuida());
+        }
+        /*Saber si alguna receta es rápida.
+        Saber si Harry es un lento.
+        Saber si Harry no se cuida
+        */
+        [Test]
+        public void TestSaberSiAlgunaRecetaEsRápida()
+        {
+            Assert.IsFalse(HarryPostre.Recetas.Any(recetaAux => recetaAux.esRapida()));
+        }
+        [Test]
+        public void TestSaberSiHarryPostreEsUnLento()
+        {
+            Assert.AreEqual(false, HarryPostre.esUnLento());
+        }
+        [Test]
+        public void TestSaberSiHarryPostreNoSeCuida()
+        {
+            Assert.AreEqual(false,HarryPostre.noSeCuida());
         }
     }
 }
